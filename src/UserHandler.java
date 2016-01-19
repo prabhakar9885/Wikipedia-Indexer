ada88a0d47241fcf41d5b2a64cee1389b8982fdb
prabhakar9885 <prabhakar.9885@gmail.com>
2016-01-19 23:41:03 +0530

    Refactored the method endElement.

--- src/UserHandler.java
+++ src/UserHandler.java
@@ -86,37 +86,21 @@ class UserHandler extends DefaultHandler {
 			// Increment the title-tokens' titleFrequeny by 1
 			pageId = Integer.parseInt(str.toString().trim());
 			pageIdSet = true;
-			PageInfo pi = null;
-			TreeMap<Integer, PageInfo> piHastTable;
-			for (String str : tokensInTitle) {
-				if (tokenTree.containsKey(str.toString().trim())) {
-					piHastTable = tokenTree.get(str.toString().trim());
-					pi = piHastTable.get(pageId);
-					if (pi == null) {
-						pi = new PageInfo();
-						pi.titleFrequeny = 1;
-						piHastTable.put(pageId, pi);
-					} else
-						pi.titleFrequeny++;
-				} else {
-					piHastTable = new TreeMap<Integer, PageInfo>();
-					pi = new PageInfo();
-					pi.titleFrequeny = 1;
-					piHastTable.put(pageId, pi);
-					tokenTree.put(str.toString().trim(), piHastTable);
-				}
-			}
+			addTokensInTitleToPageInfoTreeMap(tokensInTitle);
 			return;
-
 		}
 
-		//
 		String s1 = str.toString().toLowerCase();
 		String s = s1;
 
-		// Extract References, Categories and Infobox content
+		/*
+		 * Extract References, Categories and Infobox content
+		 */
 		if (qName.equalsIgnoreCase("text")) {
-			// Extract refs.
+
+			/*
+			 * Extract refs.
+			 */
 			temp.setLength(0);
 			s = s1.replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&quot;", " ");
 
@@ -129,68 +113,41 @@ class UserHandler extends DefaultHandler {
 				temp.append(s.substring(nextIndex, endIndex).replaceAll("(&lt;)|(&gt;)|[^a-z0-9]+", " "));
 				nextIndex = endIndex;
 			}
-
-			ArrayList<String> refTokens = UtilFuncs
-					.getTokensAsList(temp.toString().replaceAll("(&lt;)|(&gt;)|[^a-z0-9]+", " "), " ");
-
+			fillMapWithTokens(refMap, temp.toString(), "(&lt;)|(&gt;)|[^a-z0-9]+");
 			temp.setLength(0);
-			for (String item : refTokens)
-				if (refMap.containsKey(item))
-					refMap.put(item, refMap.get(item) + 1);
-				else
-					refMap.put(item, 1);
 
-			if (pageId == 6201)
-				pageId = 6201;
-
-			// Extract External Links
+			/*
+			 * Extract External Links
+			 */
 			i = s.indexOf("==external links==") + 18;
 			int categoryStartsAt = UtilFuncs.indexOf("\\[\\[category", s1.toString());
 			if (i != 17 && i < categoryStartsAt) {
-
 				temp.setLength(0);
-				String link = null;
 				while (i < categoryStartsAt) {
-					while ( i < categoryStartsAt && s.charAt(i) != '[')
+					while (i < categoryStartsAt && s.charAt(i) != '[')
 						i++;
 					i++;
 					while (i < categoryStartsAt && s.charAt(i) != ']')
 						temp.append(s.charAt(i++));
 					temp.append(" ");
 				}
-
-				ArrayList<String> extLinksTokens = UtilFuncs
-						.getTokensAsList(temp.toString().replaceAll("[^a-z0-9]+|(www)|(http:s?)", " "), " ");
-
+				fillMapWithTokens(extLinksMap, temp.toString(), "[^a-z0-9]+|(www)|(http:s?)");
 				temp.setLength(0);
-				for (String item : extLinksTokens)
-					if (extLinksMap.containsKey(item))
-						extLinksMap.put(item, extLinksMap.get(item) + 1);
-					else
-						extLinksMap.put(item, 1);
 			}
 
-			// Extract Categories
+			/*
+			 * Extract Categories
+			 */
 			if (categoryStartsAt != -1) {
 				for (i = categoryStartsAt; i < n; i++) {
 					while (i < n && s1.charAt(i) != ':' && s1.charAt(i) != ']')
 						i++;
-
 					endIndex = i;
 					while (endIndex < n && s1.charAt(endIndex) != ']')
 						endIndex++;
-
 					if (i >= n || s1.charAt(i) == ']')
 						break;
-
-					ArrayList<String> catTokens = UtilFuncs
-							.getTokensAsList(s1.substring(i, endIndex).replaceAll("[^a-z0-9]+", " "), " ");
-					for (String t : catTokens) {
-						if (categoryMap.containsKey(t))
-							categoryMap.put(t, categoryMap.get(t) + 1);
-						else
-							categoryMap.put(t, 1);
-					}
+					fillMapWithTokens(categoryMap, s1.substring(i, endIndex), "[^a-z0-9]+");
 					i = endIndex + 1;
 				}
 			}
@@ -198,7 +155,9 @@ class UserHandler extends DefaultHandler {
 			s = s.replaceAll("[^A-Za-z0-9]+", " ");
 			infoText = null;
 
-			// Extract Infobox content
+			/*
+			 * Extract Infobox content
+			 */
 			n = s1.length();
 			int infoboxStartsAt = UtilFuncs.indexOf("\\{\\{infobox", s1.toString());
 			int infoboxEndsAt = 0;
@@ -217,63 +176,106 @@ class UserHandler extends DefaultHandler {
 				infoText = s1.substring(infoboxStartsAt + len, infoboxEndsAt).replaceAll("\\n|[a-z ]+=", " ")
 						.replaceAll("[^a-z0-9]+", " ");
 
-				ArrayList<String> infoBoxTokens = UtilFuncs.getTokensAsList(infoText.replaceAll("[^a-z0-9]+", " "),
-						" ");
-				for (String t : infoBoxTokens) {
-					if (infoboxMap.containsKey(t))
-						infoboxMap.put(t, infoboxMap.get(t) + 1);
-					else
-						infoboxMap.put(t, 1);
-				}
-				infoBoxTokens.clear();
+				fillMapWithTokens(infoboxMap, infoText, "[^a-z0-9]+");
 			}
 
 		}
 
 		ArrayList<String> strList = UtilFuncs.getTokensAsList(s.replaceAll("[^a-z0-9]+", " "), " ");
 
-		for (String str : strList) {
-			str = str.trim();
-			PageInfo pi = null;
-			TreeMap<Integer, PageInfo> piHastTable;
+		PageInfo pi = null;
+		TreeMap<Integer, PageInfo> piTreeMap;
+		for (String token : strList) {
+			token = token.trim();
 
-			// Update the word frequency in the text/body.
-			if (tokenTree.containsKey(str.toString().trim())) {
-				piHastTable = tokenTree.get(str.toString().trim());
-				pi = piHastTable.get(pageId);
+			/*
+			 * Update the word frequency in the in-memory object piHastTable
+			 */
+			if (tokenTree.containsKey(token)) {
+				piTreeMap = tokenTree.get(token);
+				pi = piTreeMap.get(pageId);
 				if (pi == null) {
 					pi = new PageInfo();
 					pi.frequency = 1;
-					piHastTable.put(pageId, pi);
+					piTreeMap.put(pageId, pi);
 				} else
 					pi.frequency++;
 			} else {
-				piHastTable = new TreeMap<Integer, PageInfo>();
+				piTreeMap = new TreeMap<Integer, PageInfo>();
 				pi = new PageInfo();
 				pi.frequency = 1;
-				piHastTable.put(pageId, pi);
+				piTreeMap.put(pageId, pi);
 				if (qName.equalsIgnoreCase("title"))
-					tokensInTitle.add(str);
+					tokensInTitle.add(token);
 				else
-					tokenTree.put(str.toString().trim(), piHastTable);
+					tokenTree.put(token, piTreeMap);
 			}
 
 			if (qName.equalsIgnoreCase("text")) {
-				if (pi.infoboxFrequeny == 0 && infoboxMap.containsKey(str)) {
-					pi.infoboxFrequeny = infoboxMap.get(str);
-				}
-				if (pi.categoryFrequeny == 0 && categoryMap.containsKey(str)) {
-					pi.categoryFrequeny = categoryMap.get(str);
-				}
-				if (pi.refFrequeny == 0 && refMap.containsKey(str)) {
-					pi.refFrequeny = refMap.get(str);
-				}
-				if (pi.extLinkFrequeny == 0 && extLinksMap.containsKey(str)) {
-					pi.extLinkFrequeny = extLinksMap.get(str);
-				}
-				piHastTable.put(pageId, pi);
+				updateFrequeniesInPagInfoObj(token, pi, piTreeMap);
 			}
 		}
+	}
+
+	/**
+	 * Fills the <i>map</i> by tokenizing the <i>string</i> obtained by removing
+	 * the <i>regEx</i> pattern from the string.
+	 */
+	private void fillMapWithTokens(HashMap<String, Integer> map, String string, String regEx) {
+
+		ArrayList<String> tokensList = UtilFuncs.getTokensAsList(string.toString().replaceAll(regEx, " "), " ");
+
+		for (String item : tokensList)
+			if (map.containsKey(item))
+				map.put(item, map.get(item) + 1);
+			else
+				map.put(item, 1);
+	}
+
+	/**
+	 * Adds the tokens present in the set <i>tokensInTitle</i>
+	 */
+	private void addTokensInTitleToPageInfoTreeMap(HashSet<String> tokensInTitle) {
+		PageInfo pi;
+		TreeMap<Integer, PageInfo> piTreeMap;
+		for (String str : tokensInTitle) {
+			if (tokenTree.containsKey(str.toString().trim())) {
+				piTreeMap = tokenTree.get(str.toString().trim());
+				pi = piTreeMap.get(pageId);
+				if (pi == null) {
+					pi = new PageInfo();
+					pi.titleFrequeny = 1;
+					piTreeMap.put(pageId, pi);
+				} else
+					pi.titleFrequeny++;
+			} else {
+				piTreeMap = new TreeMap<Integer, PageInfo>();
+				pi = new PageInfo();
+				pi.titleFrequeny = 1;
+				piTreeMap.put(pageId, pi);
+				tokenTree.put(str.toString().trim(), piTreeMap);
+			}
+		}
+	}
+
+	/**
+	 * Updates the map <i>piTreeMap</i> with the info present in the PageInfo
+	 * object <i>pi</i>
+	 */
+	private void updateFrequeniesInPagInfoObj(String token, PageInfo pi, TreeMap<Integer, PageInfo> piTreeMap) {
+		if (pi.infoboxFrequeny == 0 && infoboxMap.containsKey(token)) {
+			pi.infoboxFrequeny = infoboxMap.get(token);
+		}
+		if (pi.categoryFrequeny == 0 && categoryMap.containsKey(token)) {
+			pi.categoryFrequeny = categoryMap.get(token);
+		}
+		if (pi.refFrequeny == 0 && refMap.containsKey(token)) {
+			pi.refFrequeny = refMap.get(token);
+		}
+		if (pi.extLinkFrequeny == 0 && extLinksMap.containsKey(token)) {
+			pi.extLinkFrequeny = extLinksMap.get(token);
+		}
+		piTreeMap.put(pageId, pi);
 	}
 
 	public void endDocument() throws SAXException {
