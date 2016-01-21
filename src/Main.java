2732f313b10a79a2316aed948e066392e4997c34
prabhakar9885 <prabhakar.9885@gmail.com>
2016-01-21 16:45:06 +0530

    Input taken as command-line args.

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,12 @@ public class Main {
 		long lStartTime = System.currentTimeMillis();
 		try {
 			// File inputFile = new File("../SaxExample2/res/xml.xml");
-			File inputFile = new File("/home/prabhakar/IIIT-H_current/Sem 2/IRE/Mini-Project/wiki-search-small_.xml");
+			File inputFile = new File(args[0]);
 			SAXParserFactory factory = SAXParserFactory.newInstance();
 			SAXParser saxParser = factory.newSAXParser();
 			UserHandler userhandler = new UserHandler();
 
-			userhandler.outFile = new FileWriter("/home/prabhakar/IIIT-H_current/Sem 2/IRE/Mini-Project/temp.xml");
+			userhandler.outFile = new FileWriter(args[1]);
 			userhandler.tokenTree = new TreeMap<String, TreeMap<Integer, PageInfo>>();
 
 			saxParser.parse(inputFile, userhandler);
